9b9166a27c4af8b9773d98f957862beb4365a7f4
Nikita <jadhavnikita31@gmail.com>
2024-02-16 21:21:46 -0500

    Added read_args in pipeline.py

--- src/pipeline_data_prep.py
+++ src/pipeline_data_prep.py
@@ -3,11 +3,20 @@ import argparse
 import yaml
 import logging
 
+def read_args(config_path):
+    with open(config_path) as yamlfile:
+        config_arg= yaml.safe_load(yamlfile)
+    return config_arg
+
+def main(config_path,data_source):
+    config=read_args(config_path)
+    print(config)
 
 if __name__=='__main__':
     args=argparse.ArgumentParser()
-    args.add_argument('--config',default='default')
-    args.add_argument('--arg1',default=None)
+    default_config_path=os.path.join('config','params.yaml')
+    args.add_argument('--config',default=default_config_path)
+    args.add_argument('--data_source',default=None)
 
     parser=args.parse_args()
-    print(parser.arg1, parser.config)
+    main(config_path=parser.config,data_source=parser.data_source)
